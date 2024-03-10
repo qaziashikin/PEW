a6c7d1df94c65055d8dc727de1792c3d0acc91cb
qaziashikin <qaziashikin@gmail.com>
2024-03-10 18:07:03 -0400

    Add timer and final message display

--- Assets/Scripts/ScoreSystem.cs
+++ Assets/Scripts/ScoreSystem.cs
@@ -1,17 +1,22 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI; // Required for working with UI elements
+using System.Collections;
 
 public class ScoreSystem : MonoBehaviour
 {
     // public TextMeshProUGUI scoreText; // Assign this in the inspector with your UI Text element
-    private int score = 0; // Initial score
+    public int score = 0; // Initial score
     public Text scoreBoardText;
+    private int time = 3;
+    public Text timeText; // Change this line
+    public Text finalMessageText; // Change this line
 
     // Start is called before the first frame update
     void Start()
     {
         UpdateScoreText(); // Initial score update
+        StartCoroutine(UpdateTimeText());
     }
 
     // Method to add points and update the score display
@@ -26,5 +31,29 @@ public class ScoreSystem : MonoBehaviour
     {
         scoreBoardText.text = "Score: " + score.ToString();
         // Debug.Log("Current Score: " + score);
+    }
+
+    // Update is called once per frame
+    IEnumerator UpdateTimeText()
+    {
+        while (time >= 0)
+        {
+            timeText.text = "Time: " + time.ToString();
+            yield return new WaitForSeconds(1);
+            time--; // Decrease time each second
+
+            if (time <= 0)
+            {
+                // Stop the game when time is 0
+                Time.timeScale = 0;
+                DisplayFinalMessage();
+            }
+        }
+    }
+
+    void DisplayFinalMessage()
+    {
+        finalMessageText.text = "Game over. Your final score was: " + score.ToString();
+        Application.Quit();
     }
 }
